a136d113573678ab3f2c16bb6869b3e3a83474f3
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-24 01:25:19 +0400

    10_commit

---
+++ 010_Expressions_/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _010_Expressions_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
