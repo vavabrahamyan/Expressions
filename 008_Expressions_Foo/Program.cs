1854049cb1218043824f22e1c204e6babf38d9c9
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-23 19:33:48 +0400

    008_commit

---
+++ 008_Expressions_Foo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _008_Expressions_Foo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var st = new Student() { Name = "A1", Surname = "A1yan", Age = 26 };
+
+            Expression<Func<Student, bool>> q1 = f => f.Age > 12;
+            Expression<Func<Student, bool>> q2 = f => f.Age < 20;
+
+            BinaryExpression andExp = Expression.AndAlso(q1.Body, q2.Body);
+            var parExp = Expression.Parameter(typeof(Student), "s");
+
+            Expression<Func<Student, bool>> isTeenAgerExp = Expression.Lambda<Func<Student, bool>>(andExp, parExp);
+            Func<Student, bool> isTeenAger = isTeenAgerExp.Compile();
+            if (isTeenAger(st))
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 008_Expressions_Foo/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _008_Expressions_Foo
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
