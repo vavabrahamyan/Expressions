ff84bad78228c72584f30099890ec5d3963766a4
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-23 19:01:15 +0400

    06_commit

---
+++ 001_Expressions/006_Expression/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _006_Expression
+{
+    class Student
+    {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+
+        public void Show()
+        {
+            Console.WriteLine(this.Name + "  " + this.Surname);
+        }
+    }
+}
