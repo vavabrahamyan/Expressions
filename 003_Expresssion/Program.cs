daf487f604634202ae4eb89013f14763ca5cb495
Vahe Abrahamyan <vavabrahamyan@gmail.com>
2020-02-23 17:39:43 +0400

    03_Commit

---
+++ 003_Expresssion/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace _003_Expresssion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Expression<Action<Student>> studentsNameExpr = s => Console.WriteLine(s.Fullname);
+
+            var st = new Student("Gag", "Gagyan", 24);
+
+            Action<Student> printFullName = studentsNameExpr.Compile();
+            printFullName.Invoke(st);
+
+            Console.ReadLine();
+        }
+    }
+}
